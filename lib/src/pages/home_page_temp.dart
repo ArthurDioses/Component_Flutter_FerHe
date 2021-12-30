bbb3852dea8f07a820774ab5dd8885c73cc4027d
adiosesr <Peru123456>
2021-12-29 20:22:48 -0500

    -Create list dinamic with dividers

--- lib/src/pages/home_page_temp.dart
+++ lib/src/pages/home_page_temp.dart
@@ -1,27 +1,33 @@
 import 'package:flutter/material.dart';
 
 class HomePageTemp extends StatelessWidget {
+  final opciones = ['Uno', 'Dos', 'Tres', 'Cuatro', 'Cinco'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Componentes Temp'),
       ),
-      body: ListView(
-        children: <Widget>[
-          ListTile(
-            title: Text('List Title'),
-          ),
-          Divider(),
-          ListTile(
-            title: Text('List Title'),
-          ),
-          Divider(),
-          ListTile(
-            title: Text('List Title'),
-          )
-        ],
-      ),
+      body: ListView(children: _crearItems()),
     );
+  }
+
+  List<Widget> _crearItems() {
+    List<Widget> lista = <Widget>[];
+    for (String opt in opciones) {
+      final tempWidgte = ListTile(
+        title: Text(opt),
+      );
+      lista
+        ..add(tempWidgte)
+        ..add(Divider(height: 5.0, thickness: 0.3, color: Colors.blue));
+    }
+
+    return lista;
+  }
+
+  List<Widget> _crearItemsCorto() {
+    return [];
   }
 }
