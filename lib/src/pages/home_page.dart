ec05d5ff54c1608957debe5201bc31171d771538
Arthur Dioses <adiosesr@proveedor.intercorp.com.pe>
2023-03-11 22:37:40 -0500

    Add pages to navigate and go from home page to Alert Page

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -2,6 +2,8 @@ import 'package:component_flutter_ferhe/src/providers/menu_provider.dart';
 import 'package:component_flutter_ferhe/src/utils/icono_string_util.dart';
 import 'package:flutter/material.dart';
 
+import 'package:component_flutter_ferhe/src/pages/alert_page.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -21,20 +23,23 @@ class HomePage extends StatelessWidget {
       //initialData: [],
       builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
         return ListView(
-          children: _listaItems(snapshot.data),
+          children: _listaItems(snapshot.data, context),
         );
       },
     );
   }
 
-  List<Widget> _listaItems(List<dynamic>? data) {
+  List<Widget> _listaItems(List<dynamic>? data, BuildContext context) {
     final List<Widget> opciones = [];
     data!.forEach((opt) {
       final widgetTemp = ListTile(
         title: Text(opt['texto']),
         leading: getIcon(opt['icon']),
         trailing: Icon(Icons.keyboard_arrow_right, color: Colors.blue),
-        onTap: () {},
+        onTap: () {
+          final route = MaterialPageRoute(builder: (context) => AlertPage());
+          Navigator.push(context, route);
+        },
       );
       opciones
         ..add(widgetTemp)
