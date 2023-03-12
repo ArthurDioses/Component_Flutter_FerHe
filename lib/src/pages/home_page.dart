77dddbbd30fe8438d0cfcdfe35e0ad5eeb4223de
Arthur Dioses <adiosesr@proveedor.intercorp.com.pe>
2023-03-11 21:56:37 -0500

    Add json and list 3 items

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:component_flutter_ferhe/src/providers/menu_provider.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -14,20 +15,30 @@ class HomePage extends StatelessWidget {
   }
 
   Widget _lista() {
-    return ListView(
-      children: _listaItems(),
+    return FutureBuilder(
+      future: menuProvider.cargarData(),
+      //initialData: [],
+      builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
+        return ListView(
+          children: _listaItems(snapshot.data),
+        );
+      },
     );
   }
 
-  List<Widget> _listaItems() {
-    return [
-      ListTile(title: Text('Hola mundo cruel 1')),
-      Divider(),
-      ListTile(title: Text('Hola mundo cruel 2')),
-      Divider(),
-      ListTile(title: Text('Hola mundo cruel 3')),
-      Divider(),
-      ListTile(title: Text('Hola mundo cruel 4')),
-    ];
+  List<Widget> _listaItems(List<dynamic>? data) {
+    final List<Widget> opciones = [];
+    data!.forEach((opt) {
+      final widgetTemp = ListTile(
+        title: Text(opt['texto']),
+        leading: Icon(Icons.adb, color: Colors.blue),
+        trailing: Icon(Icons.keyboard_arrow_right, color: Colors.blue),
+        onTap: () {},
+      );
+      opciones
+        ..add(widgetTemp)
+        ..add(Divider());
+    });
+    return opciones;
   }
 }
