476a06a1a60473ad25e1f164fa6e60489833b869
Arthur Dioses <adiosesr@proveedor.intercorp.com.pe>
2023-03-20 23:03:51 -0500

    InitialData with empty

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -20,7 +20,7 @@ class HomePage extends StatelessWidget {
   Widget _lista() {
     return FutureBuilder(
       future: menuProvider.cargarData(),
-      //initialData: [],
+      initialData: [],
       builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
         return ListView(
           children: _listaItems(snapshot.data, context),
