99440108ea0f2a70c84b4e629ff0d2265bdd6048
Arthur Dioses <adiosesr@proveedor.intercorp.com.pe>
2023-03-22 00:32:50 -0500

    Customize styles in the cards

--- lib/src/pages/card_page.dart
+++ lib/src/pages/card_page.dart
@@ -14,7 +14,74 @@ class CardPage extends StatelessWidget {
           SizedBox(
             height: 30,
           ),
-          _cardTipo2()
+          _cardTipo2(),
+          SizedBox(
+            height: 30.0,
+          ),
+          _cardTipo1(),
+          SizedBox(
+            height: 30,
+          ),
+          _cardTipo2(),
+          SizedBox(
+            height: 30.0,
+          ),
+          _cardTipo1(),
+          SizedBox(
+            height: 30,
+          ),
+          _cardTipo2(),
+          SizedBox(
+            height: 30.0,
+          ),
+          _cardTipo1(),
+          SizedBox(
+            height: 30,
+          ),
+          _cardTipo2(),
+          SizedBox(
+            height: 30.0,
+          ),
+          _cardTipo1(),
+          SizedBox(
+            height: 30,
+          ),
+          _cardTipo2(),
+          SizedBox(
+            height: 30.0,
+          ),
+          _cardTipo1(),
+          SizedBox(
+            height: 30,
+          ),
+          _cardTipo2(),
+          SizedBox(
+            height: 30.0,
+          ),
+          _cardTipo1(),
+          SizedBox(
+            height: 30,
+          ),
+          _cardTipo2(),
+          SizedBox(
+            height: 30.0,
+          ),
+          _cardTipo1(),
+          SizedBox(
+            height: 30,
+          ),
+          _cardTipo2(),
+          SizedBox(
+            height: 30.0,
+          ),
+          _cardTipo1(),
+          SizedBox(
+            height: 30,
+          ),
+          _cardTipo2(),
+          SizedBox(
+            height: 30.0,
+          ),
         ],
       ),
     );
@@ -22,6 +89,8 @@ class CardPage extends StatelessWidget {
 
   Widget _cardTipo1() {
     return Card(
+      elevation: 10.0,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
       child: Column(
         children: <Widget>[
           ListTile(
@@ -43,7 +112,8 @@ class CardPage extends StatelessWidget {
   }
 
   Widget _cardTipo2() {
-    return Card(
+    final card = Container(
+      //clipBehavior: Clip.antiAlias,
       child: Column(
         children: <Widget>[
           FadeInImage(
@@ -64,6 +134,22 @@ class CardPage extends StatelessWidget {
           Container(
               padding: EdgeInsets.all(10.0), child: Text('No se qué poner'))
         ],
+      ),
+    );
+    return Container(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30.0),
+          color: Colors.white,
+          boxShadow: <BoxShadow>[
+            BoxShadow(
+                color: Colors.black26,
+                blurRadius: 10.0,
+                spreadRadius: 2.0,
+                offset: Offset(2.0, 10.0))
+          ]),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(30.0),
+        child: card,
       ),
     );
   }
