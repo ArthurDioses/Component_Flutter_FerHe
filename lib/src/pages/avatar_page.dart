caca2e80ade224c1576b46be5192cdbce1ef66cd
Arthur Dioses <adiosesr@proveedor.intercorp.com.pe>
2023-03-22 22:46:00 -0500

    Widget circle avatar with network image and circle avatar

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -6,6 +6,31 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.all(5.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://w7.pngwing.com/pngs/946/841/png-transparent-ironman-helmet-ironman-icon-thumbnail.png'),
+              radius: 25.0,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text('SL'),
+              backgroundColor: Colors.lightGreen,
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          image: NetworkImage(
+              'https://cdn.britannica.com/49/182849-050-4C7FE34F/scene-Iron-Man.jpg'),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          fadeInDuration: Duration(milliseconds: 200),
+        ),
       ),
     );
   }
