9c5739083f81f2c862852aa3442438de6c7c81a8
Arthur Dioses <adiosesr@proveedor.intercorp.com.pe>
2023-03-22 22:31:21 -0500

    Widget alert with borders and buttons

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -7,11 +7,51 @@ class AlertPage extends StatelessWidget {
         appBar: AppBar(
           title: Text('Alert Page'),
         ),
+        body: Center(
+          child: RaisedButton(
+            child: Text('Mostrar alerta'),
+            color: Colors.blue,
+            onPressed: () => _mostrarAlert(context),
+            shape: StadiumBorder(),
+          ),
+        ),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add_location),
           onPressed: () {
             Navigator.pop(context);
           },
         ));
+  }
+
+  void _mostrarAlert(BuildContext context) {
+    showDialog(
+        context: context,
+        barrierDismissible: true,
+        builder: (context) {
+          return AlertDialog(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0)),
+            title: Text('Titulo'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Text('Este es el contenido de la caja de la alerta'),
+                FlutterLogo(
+                  size: 100.0,
+                )
+              ],
+            ),
+            actions: <Widget>[
+              FlatButton(
+                  child: Text('Cancelar'),
+                  onPressed: () => Navigator.of(context).pop()),
+              FlatButton(
+                  child: Text('Ok'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  })
+            ],
+          );
+        });
   }
 }
