94ebe24baf58771a662b011c2ab562979ab9eed5
Arthur Dioses <adiosesr@proveedor.intercorp.com.pe>
2023-03-24 00:29:47 -0500

    Add Widget Checkbox, ChecboxListTile and Switch, SwitchListTile

--- lib/src/pages/slider_page.dart
+++ lib/src/pages/slider_page.dart
@@ -7,6 +7,7 @@ class SliderPage extends StatefulWidget {
 
 class _SliderPageState extends State<SliderPage> {
   double _valorSlider = 100.0;
+  bool _bloquearCheck = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,7 +17,12 @@ class _SliderPageState extends State<SliderPage> {
       body: Container(
         padding: EdgeInsets.only(top: 50.0),
         child: Column(
-          children: <Widget>[_crearSlider(), Expanded(child: _crearImagen())],
+          children: <Widget>[
+            _crearSlider(),
+            _crearCheckbox(),
+            _crearSwitch(),
+            Expanded(child: _crearImagen())
+          ],
         ),
       ),
     );
@@ -30,9 +36,41 @@ class _SliderPageState extends State<SliderPage> {
         value: _valorSlider,
         min: 10.0,
         max: 400.0,
+        onChanged: (_bloquearCheck)
+            ? null
+            : (valor) {
+                setState(() {
+                  _valorSlider = valor;
+                });
+              });
+  }
+
+  Widget _crearCheckbox() {
+    /*return Checkbox(
+        value: _bloquearCheck,
         onChanged: (valor) {
           setState(() {
-            _valorSlider = valor;
+            _bloquearCheck = valor!;
+          });
+        });
+        */
+    return CheckboxListTile(
+        title: Text('Bloquear Slider'),
+        value: _bloquearCheck,
+        onChanged: (valor) {
+          setState(() {
+            _bloquearCheck = valor!;
+          });
+        });
+  }
+
+  Widget _crearSwitch() {
+    return SwitchListTile(
+        title: Text('Switch'),
+        value: _bloquearCheck,
+        onChanged: (valor) {
+          setState(() {
+            _bloquearCheck = valor;
           });
         });
   }
