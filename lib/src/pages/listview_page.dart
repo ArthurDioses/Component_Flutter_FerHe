8d4f18371a6de7cf14f9ab2b63358212591919b2
Arthur Dioses <adiosesr@proveedor.intercorp.com.pe>
2023-04-02 01:52:54 -0500

    Infinited Scroll for images

--- lib/src/pages/listview_page.dart
+++ lib/src/pages/listview_page.dart
@@ -6,7 +6,23 @@ class ListaPage extends StatefulWidget {
 }
 
 class _ListaPageState extends State<ListaPage> {
-  List<int> _listanumeros = [1, 2, 3, 4, 5];
+  ScrollController _scrollController = new ScrollController();
+
+  List<int> _listanumeros = [];
+  int _ultimoItem = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _agregar10();
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
+        _agregar10();
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,6 +34,7 @@ class _ListaPageState extends State<ListaPage> {
 
   Widget _crearLista() {
     return ListView.builder(
+        controller: _scrollController,
         itemCount: _listanumeros.length,
         itemBuilder: (BuildContext context, int index) {
           final imagen = _listanumeros[index];
@@ -26,5 +43,13 @@ class _ListaPageState extends State<ListaPage> {
               image: NetworkImage(
                   'https://picsum.photos/500/300/?image=${imagen}'));
         });
+  }
+
+  void _agregar10() {
+    for (var i = 1; i < 10; i++) {
+      _ultimoItem++;
+      _listanumeros.add(_ultimoItem);
+    }
+    setState(() {});
   }
 }
